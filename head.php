c21ba0e3b4ad11ddd2622c3545e3f9bfd8c09d18
bocie <bojaboci@gmail.com>
2024-05-22 21:53:00 +0200

    so far what ive done

---
+++ head.php
@@ -0,0 +1,38 @@
+<meta charset="utf-8">
+<meta content="width=device-width, initial-scale=1.0" name="viewport">
+<title>Origo jobs</title>
+<meta content="Origo jobs" name="description">
+<meta content="job jobs work" name="keywords">
+
+<!-- Favicons -->
+<link href="assets/img/logo.png" rel="icon">
+<link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+<!-- Google Fonts -->
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,600;1,700&family=Poppins:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&family=Inter:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet">
+
+<!-- Vendor CSS Files -->
+
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
+<link href="assets/vendor/fontawesome-free/css/all.min.css" rel="stylesheet">
+<link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+<link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+<link href="assets/vendor/aos/aos.css" rel="stylesheet">
+
+<!-- Template Main CSS File -->
+<link href="assets/css/main.css" rel="stylesheet">
+<link href="assets/css/custom.css" rel="stylesheet">
+
+<!-- 
+=======================================================
+* Template Name: Logis
+* Updated: Jul 27 2023 with Bootstrap v5.3.1
+* Template URL: https://bootstrapmade.com/logis-bootstrap-logistics-website-template/
+* Author: BootstrapMade.com
+* License: https://bootstrapmade.com/license/
+======================================================== 
+-->
---
+++ index.php
@@ -0,0 +1,341 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Praksa</title>
+	<?php include('head.php'); ?>	
+</head>
+<body>
+	<?php include('menu.php'); ?>
+	<main id="main" data-bs-spy="scroll" data-bs-target="#navbar">
+	
+		<!-- ======= Featured Services Section ======= -->
+		<section id="featured-services" class="featured-services">
+			<div class="container">
+
+				<div class="row gy-4">
+
+					<div class="col-lg-4 col-md-6 service-item d-flex" data-aos="fade-up">
+				</div>
+			</div>
+		</section><!-- End Featured Services Section -->
+		<!--<button onclick="toggleDarkMode()">Toggle Dark Mode</button>-->
+		
+		<!-- ======= Testimonials Section ======= -->
+		<div>
+
+  <div class="newrow-nav">
+    
+  <div>
+    <ul>
+    <a href="#" class="newrow-head">O NAMA</a>
+    <a href="#" class="newrow-head">DVG PROIZVODI</a>
+    <a href="#" class="newrow-head"><span>BRENDOVI</span></a>
+	<a href="#" class="newrow-head">DISTRIBUCIJA</a>
+    <a href="#" class="newrow-head">SARADNJA</a>
+    <a href="#" class="newrow-head"><span>NOVOSTI</span></a>
+	<a href="#" class="newrow-head"><span>KONTAKT</span></a>
+
+    </ul>
+    
+    </div>
+   
+  </div>
+  
+</div>
+<section id="testimonials" class="testimonials">
+			
+			<div class="container">
+
+				<div class="slides-1 swiper" data-aos="fade-up">
+					<div class="swiper-wrapper">
+						<div class="swiper-slide">
+							<div class="testimonial-item">
+								<img src="assets/img/testimonials/testimonials-2.jpg" class="testimonial-img" alt="">
+					</div>
+						</div><!-- End testimonial item -->
+
+					</div>
+					<div class="swiper-pagination"></div>
+				</div>
+
+			</div>
+		</section><!-- End Testimonials Section -->
+
+		<!-- ======= Features Section ======= -->
+		<section id="features" class="features">
+			<div class="container">
+
+				<div class="row gy-4 align-items-center features-item" data-aos="fade-up">
+
+					
+				</div><!-- Features Item -->
+
+				<div class="row gy-4 align-items-center features-item" data-aos="fade-up">
+					
+				</div><!-- Features Item -->
+
+				<div class="row gy-4 align-items-center features-item" data-aos="fade-up">
+					<div class="col-md-5">
+						<img src="assets/img/features-3.jpg" class="new-img" alt="">
+					</div>
+					<div class="col-md-7">
+						<h3>DVG Company</h3>
+						<hr class="new2">
+						<p class="velep">Veleprodaja i maloprodaja kozmetike i kućne hemije</p>
+						<hr class="new1">
+						<p class="velepsz"> DVG Company d.o.o. je trgovinsko preduzeće osnovano 2008. godine u Subotici.
+						Prvenstvena delatnost kompanije je veleprodaja kozmetike i kućne hemije.
+						U potpunosti je u privatnom vlasništvu, sa sedištem u Subotici. Tu se nalazi distrubutivni centar i poslovne prostorije, sa ukupno 3000m2 magacinskog prostora koji zadovoljava sve segmente u pogledu smeštaja, čuvanja i plasiranja robe.</p>
+							
+						<button class="button button3">Saznajte više o nama</button>
+					</div>
+				</div><!-- Features Item -->
+
+				<div class="row gy-4 align-items-center features-item" data-aos="fade-up">
+					
+				</div><!-- Features Item -->
+
+			</div>
+		</section><!-- End Features Section -->
+<!-- ======= Call To Action Section ======= -->
+<section id="call-to-action" class="call-to-action">
+			<div class="container" data-aos="zoom-out">
+
+				<div class="row justify-content-center">
+					<div class="col-lg-8 text-center">
+						<h3>POUZDANA RESTRIBUCIJA</h3>
+						
+					</div>
+				</div>
+
+			</div>
+		</section><!-- End Call To Action Section -->
+		<!-- ======= Services Section ======= -->
+		<section id="service" class="services pt-0">
+			<div class="container" data-aos="fade-up">
+				<div class="row gy-4">
+
+					<div class="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="400">
+						<div class="card">
+							<div class="card-img">
+								<img src="assets/img/trucking-service.jpg" alt="" class="img-fluid">
+								
+							</div>
+							<p class="card-items">POSTANITE NAS PARTNER</p>
+							<hr class="new3">
+							</div>
+							
+					</div><!-- End Card Item -->
+
+					<div class="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="500">
+						<div class="card">
+							<div class="card-img">
+								<img src="assets/img/packaging-service.jpg" alt="" class="img-fluid">
+							</div>
+							<p class="card-items">NASI PROIZVODI</p>
+							<hr class="new3">
+							</div>
+					</div><!-- End Card Item -->
+
+					<div class="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="600">
+						<div class="card">
+							<div class="card-img">
+								<img src="assets/img/warehousing-service.jpg" alt="" class="img-fluid">
+							</div>
+							<p class="card-items">UVOZ IZ MADARSKE</p>
+							<hr class="new3">
+							</div>
+					</div><!-- End Card Item -->
+					<div class="section-header">
+					<p class="dvgnegy">DVG Company</p>
+					<p class="dvgbroj">u brojkama</p>
+					
+
+				</div>
+					
+				</div>
+				
+			</div>
+		</section><!-- End Services Section -->
+
+		<!-- ======= Hero Section ======= -->
+	<section id="hero" class="hero d-flex align-items-center mt-4">
+		<div class="container">
+			<div class="row gy-4 d-flex justify-content-between">
+				<div class="col-lg-6 order-2 order-lg-1 d-flex flex-column justify-content-center">
+					<div class="row gy-4" data-aos="fade-up" data-aos-delay="400">
+					</div>					
+				</div>
+				<div class="col-lg-5 order-1 order-lg-2 hero-img" data-aos="zoom-out">
+					
+				</div>
+					<div class="col-lg-3 col-6">
+							<div class="stats-item text-center">
+								<span>3.000m²</span>
+								<p>Magacinskog prostora</p>
+							</div>
+						</div><!-- End Stats Item -->
+						<div class="col-lg-3 col-6">
+							<div class="stats-item text-center w-100 h-100">
+								<span>20</span>
+								<p>Vozila</p>
+							</div>
+						</div><!-- End Stats Item -->
+						
+
+						<div class="col-lg-3 col-6">
+							<div class="stats-item text-center w-100 h-100">
+								<span>8</span>
+								<p>Komercijalista</p>
+							</div>
+						</div><!-- End Stats Item -->
+
+						<div class="col-lg-3 col-6">
+						
+						<div class="stats-item text-center w-100 h-100">
+						<span>170</span>
+								<p>Brendova</p>
+							</div>
+							
+						</div><!-- End Stats Item -->
+			</div>
+		</div>
+	</section><!-- End Hero Section -->
+	<div class="nasiproizvodi">
+	<!--<div class="col-md-7">
+	<h3>Nasi proizvodi</h3>
+	<hr class="new1">
+	</div> -->
+	</div>
+	<!-- ======= Services Section ======= -->
+	<section id="service" class="services pt-0">
+			<div class="container" data-aos="fade-up">
+
+			<div class="section-header">
+					
+
+				</div>
+
+				<div class="row gy-4">
+
+					<!--<div class="col-lg-1 col-md-5">
+						<div class="card">
+							<div class="card-img">
+								<img src="assets/img/storage-service.jpg" alt="" class="img-fluid">
+							</div>
+						</div>
+					</div> End Card Item -->
+
+					<!--<div class="col-lg-1 col-md-5">
+						<div class="card">
+							<div class="card-img">
+								<img src="assets/img/logistics-service.jpg" alt="" class="img-fluid">
+							</div>
+						</div>
+					</div> End Card Item -->
+
+					<!--<div class="col-lg-1 col-md-5">
+						<div class="card">
+							<div class="card-img">
+								<img src="assets/img/cargo-service.jpg" alt="" class="img-fluid">
+							</div>
+						</div>
+					</div> End Card Item -->
+					<!--<div class="col-lg-1 col-md-5">
+						<div class="card">
+							<div class="card-img">
+								<img src="assets/img/storage-service.jpg" alt="" class="img-fluid">
+							</div>
+						</div>
+					</div> End Card Item -->
+
+					<!--<div class="col-lg-1 col-md-5">
+						<div class="card">
+							<div class="card-img">
+								<img src="assets/img/logistics-service.jpg" alt="" class="img-fluid">
+							</div>
+						</div>
+					</div> End Card Item -->
+
+					<!--<div class="col-lg-1 col-md-5">
+						<div class="card">
+							<div class="card-img">
+								<img src="assets/img/cargo-service.jpg" alt="" class="img-fluid">
+								
+							</div>
+						
+							</div>
+					</div> End Card Item -->
+
+</div>
+
+			</div>
+		</section><!-- End Services Section -->
+		<div class="lstbttn">  
+<!-- <button> Svi brendovi u nasem portfoliju </button>  -->
+</div> 
+<section id="testimonials" class="testimonials">
+			
+			<div class="container">
+
+				<div class="slides-1 swiper" data-aos="fade-up">
+					<div class="swiper-wrapper">
+
+						<div class="swiper-slide">
+							<div class="testimonial-item">
+							<img src="assets/img/testimonials/kese.png" class="testimonial-img" alt="">
+							<img src="assets/img/testimonials/kuhinju.png" class="testimonial-img" alt="">
+							<img src="assets/img/testimonials/papir.png" class="testimonial-img" alt="">
+							<img src="assets/img/testimonials/kozmetika.png" class="testimonial-img" alt="">
+							<img src="assets/img/testimonials/kante.png" class="testimonial-img" alt="">
+								
+								<p>
+									
+								</p>
+							</div>
+						</div><!-- End testimonial item -->
+
+						<div class="swiper-slide">
+							<div class="testimonial-item">
+								<img src="assets/img/testimonials/testimonials-2.jpg" class="testimonial-img" alt="">
+								
+								
+							</div>
+						</div><!-- End testimonial item -->
+
+						
+
+					</div>
+					<div class="swiper-pagination"></div>
+				</div>
+
+			</div>
+		</section><!-- End Testimonials Section -->
+
+
+	</main><!-- End #main -->
+
+	<?php include('footer.php'); ?>
+
+	<a href="#" class="scroll-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+
+
+	<div id="preloader"></div>
+
+	<!-- Vendor JS Files -->
+	
+	<script src="assets/vendor/purecounter/purecounter_vanilla.js"></script>
+	<script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+	<script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+	<script src="assets/vendor/aos/aos.js"></script>
+	<script src="assets/vendor/php-email-form/validate.js"></script>
+
+	<!-- Template Main JS File -->
+	<script src="assets/js/main.js"></script>
+	<script src="assets/js/jobSearch.js"></script>
+</body>
+</html>
