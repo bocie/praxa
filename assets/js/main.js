579a61a30703faaf0f8a006bb1952ac578555463
bocie <bojaboci@gmail.com>
2024-05-28 00:58:16 +0200

    All tasks that i have done in the first week

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+function myFunction() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+  function darkmode() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+ }
