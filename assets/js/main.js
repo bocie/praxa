f8ce3a6abd4b71e2190db5ccfbd506bb68e5e5ca
bocie <bojaboci@gmail.com>
2024-05-29 00:28:26 +0200

    almost done with styling, popup working, hamburger menu and responsiveness in progress

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,41 @@
-function myFunction() {
-    var x = document.getElementById("myLinks");
-    if (x.style.display === "block") {
-      x.style.display = "none";
-    } else {
-      x.style.display = "block";
-    }
+document.addEventListener('DOMContentLoaded', function() {
+  let scrollTimeout;
+  let popupDisplayed = false;
+
+  window.addEventListener('scroll', function() {
+      if (!popupDisplayed) {
+          clearTimeout(scrollTimeout);
+          scrollTimeout = setTimeout(showPopup, 500); // Show popup after half second of scrolling
+      }
+  });
+
+  function showPopup() {
+      popupDisplayed = true;
+      document.getElementById('popup').style.display = 'block';
+      document.body.style.overflow = 'hidden'; // Disable further scrolling
   }
 
-  function darkmode() {
-    var element = document.body;
-    element.classList.toggle("dark-mode");
- }
+  document.getElementById('close-popup').addEventListener('click', closePopup);
+  document.getElementById('popup-button').addEventListener('click', closePopup);
+
+  function closePopup() {
+      document.getElementById('popup').style.display = 'none';
+      document.body.style.overflow = 'auto'; // Enable scrolling again
+  }
+});
+
+
+
+function toggleDarkMode() {
+  var element = document.body;
+  element.classList.toggle("dark-mode");
+}
+
+ function toggleMenu() {
+  const navUl = document.querySelector('.navul');
+  if (navUl.style.display === 'block') {
+      navUl.style.display = 'none';
+  } else {
+      navUl.style.display = 'block';
+  }
+}
