f534220add907aa7a1730f1a841998d2b490f02d
bocie <bojaboci@gmail.com>
2024-05-29 22:22:11 +0200

    popup and dark/light mode fully implemented, hamburger menu aswell, responsiveness in progress

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,41 +1,39 @@
 document.addEventListener('DOMContentLoaded', function() {
-  let scrollTimeout;
-  let popupDisplayed = false;
-
-  window.addEventListener('scroll', function() {
+    setTimeout(showPopup, 4000); // Show popup after 4 seconds of page load
+  
+    let scrollTimeout;
+    let popupDisplayed = false;
+  
+    function showPopup() {
       if (!popupDisplayed) {
-          clearTimeout(scrollTimeout);
-          scrollTimeout = setTimeout(showPopup, 500); // Show popup after half second of scrolling
+        popupDisplayed = true;
+        document.getElementById('popup').style.display = 'block';
+        document.body.style.overflow = 'hidden'; // Disable further scrolling
       }
-  });
-
-  function showPopup() {
-      popupDisplayed = true;
-      document.getElementById('popup').style.display = 'block';
-      document.body.style.overflow = 'hidden'; // Disable further scrolling
-  }
-
-  document.getElementById('close-popup').addEventListener('click', closePopup);
-  document.getElementById('popup-button').addEventListener('click', closePopup);
-
-  function closePopup() {
+    }
+  
+    document.getElementById('close-popup').addEventListener('click', closePopup);
+    document.getElementById('popup-button').addEventListener('click', closePopup);
+  
+    function closePopup() {
       document.getElementById('popup').style.display = 'none';
       document.body.style.overflow = 'auto'; // Enable scrolling again
-  }
-});
+    }
+  });
 
 
 
 function toggleDarkMode() {
   var element = document.body;
   element.classList.toggle("dark-mode");
+  
 }
 
- function toggleMenu() {
-  const navUl = document.querySelector('.navul');
-  if (navUl.style.display === 'block') {
-      navUl.style.display = 'none';
-  } else {
-      navUl.style.display = 'block';
+function Hamburgermenu() {
+    var x = document.getElementById("myTopnavig");
+    if (x.className === "topnavig") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnavig";
+    }
   }
-}
